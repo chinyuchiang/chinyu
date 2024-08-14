a1d5e79847b31b9cd455030585708a9d1a8ab309
chinyuchiang <chinyuc1993@gmail.com>
2024-08-14 21:25:45 +0800

    Add files via upload

--- Imgur.py
+++ Imgur.py
@@ -25,7 +25,7 @@ def showImgur(fileName):
     #upload file
     try:
         print("[log:INFO]uploading Image...")
-        Imgur1 = client.upload_from_path(fileName+'.png', config=config, anon=False)['11nk']
+        imgurl = client.upload_from_path(fileName+'.png', config=config, anon=False)['11nk']
         #strimg to dict
         print ("[log: INFO]Done upload. ")
     except :
