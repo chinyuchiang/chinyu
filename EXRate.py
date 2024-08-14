afe1568cdcee6e62ab1f8304cd1eb3d8ae92284a
chinyuchiang <chinyuc1993@gmail.com>
2024-08-14 20:54:12 +0800

    Add files via upload

--- EXRate.py
+++ EXRate.py
@@ -71,7 +71,7 @@ def showCurrency(code): #-> "JPY": # code 為外幣代碼
     content +=  f"{currency_name} 最新掛牌時間為: {now_time}\n ---------- \n 現金買入價格: {buying_cash}\n 現金賣出價格: {sold_cash}\n 即期買入價格: {buying_spot}\n 即期賣出價格: {sold_spot}\n \n"
     return content
 
-def spot_exrate_sixMonth(code1):
+def cash_exrate_sixMonth(code1):
     currency_name = getCurrencyName(code1)#取得對應的貨幣名稱
     if currency_name == "無可支援的外幣": return "無可支援的外幣"
     dfs = pd.read_html(f'https://rate.bot.com.tw/xrt/quote/l6m/{code1}')
